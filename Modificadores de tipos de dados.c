5c9de4effc3df02cd79f5fe237bec71775f122de
Jean Castro <jean.castro.42@gmail.com>
2026-02-05 16:09:08 -0300

    usando usigned e long

---
+++ Modificadores de tipos de dados.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+int main(int argc, char const *argv[]){
+    unsigned long int numerograndepositivo = 4000000000;
+    unsigned int numeropositivo = 4000000000;
+    long long int numerogrande = 40000000000;
+    int numero = 4000000000;
+
+    printf("unsigned long int: %lu\n", numerograndepositivo);
+    printf("unsigned int: %u\n", numeropositivo);
+    printf("long long int: %lld\n", numerogrande);
+    printf("int: %d\n", numero);
+
+
+    return 0;
+}
