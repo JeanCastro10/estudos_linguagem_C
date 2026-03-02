7a741e907c62f700455c52da06f8550fc8a6c00e
JeanCastro10 <jean.castro.42@gmail.com>
2026-03-02 12:00:08 -0300

    commit array.c

--- recursividade.c
+++ recursividade.c
@@ -1,16 +1,18 @@
 #include <stdio.h>
 
-void recurnsive (int numero){
-    if (numero > 0)
+void recursive (int numero){
+    if (numero >= 0)
     {
-        printf("%d", numero);
-        recurnsive (numero - 1);
+        printf("%d\n", numero);
+        recursive (numero - 1);
+        
     }
 }
 
 int main () {
     int contagem = 10;
-    printf("contagem regressiva\n");
-    recurnsive (contagem);
+        printf("contagem regressiva\n");
+        recursive (contagem);
+   
     return 0;
 }
